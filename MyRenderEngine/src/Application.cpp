a3083d319d7294443a8a1ea38eaf483eea1b4800
Javinigadela <javiserrano97@hotmail.es>
2021-11-30 00:44:35 +0100

    Basic WASD camera control

--- MyRenderEngine/src/Application.cpp
+++ MyRenderEngine/src/Application.cpp
@@ -12,6 +12,15 @@
 
 using namespace std;
 
+//VARIABLES
+// Camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+
+
+
 // Funtion headers
 void windowResize(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -192,16 +201,9 @@ int main(void)
     
 
     // Camera
-    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); // trick to get the right vector
-    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
     glm::mat4 view;
-    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),  // Camera pos
-                       glm::vec3(0.0f, 0.0f, 0.0f),  // Camera target
-                       glm::vec3(0.0f, 1.0f, 0.0f)); // Up vector in world space
+    view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
     const float radius = 10.0f;
 
 
@@ -244,9 +246,7 @@ int main(void)
         ourShader.setColor("ourColor", 0.0f, 0.0f, 1.0f);
 
         // Camera
-        float camX = sin(glfwGetTime()) * radius;
-        float camZ = cos(glfwGetTime()) * radius;
-        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         ourShader.setMat4("view", view);
 
         glBindVertexArray(VAO);
@@ -291,4 +291,16 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    // Camera control
+    const float cameraSpeed = 0.05f; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
 }
